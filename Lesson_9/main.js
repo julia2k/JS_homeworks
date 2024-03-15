8f674ec343ff2dd99bcd7dbd024ba5093ef00623
julia2k <yuliana.kutepova@gmail.com>
2024-03-15 12:17:26 -0500

    added homework for lesson 9 js

---
+++ Lesson_9/main.js
@@ -0,0 +1,262 @@
+
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+let block = document.createElement('div');
+block.classList.add('wrap','collapse','alpha','beta');
+document.body.appendChild(block);
+
+// - клонувати його повністю, та додати клон в body.
+let clone = block.cloneNode(true);
+document.body.appendChild(clone);
+
+//
+// - Є масив:
+let arr = ['Main','Products','About us','Contacts'];
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+let ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+for (const liElement in arr) {
+    let li = document.createElement('li');
+        li.innerText = arr[liElement];
+        ul.appendChild(li);
+}
+
+
+// =========================
+//
+// - Є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+
+let wrap = document.createElement('div');
+document.body.appendChild(wrap);
+for (const courseElement of coursesAndDurationArray) {
+    let course = document.createElement('div');
+    course.innerHTML = `
+        <h2>${courseElement.title}</h2>
+        <p>Довжина курсу - ${courseElement.monthDuration} мiсяцiв</p>
+        <hr>
+    `
+   wrap.appendChild(course);
+}
+
+
+// =========================
+//
+//     - Є масив
+let coursesAndDurationArray2 = [
+     {title: 'JavaScript Complex', monthDuration: 5},
+     {title: 'Java Complex', monthDuration: 6},
+     {title: 'Python Complex', monthDuration: 6},
+     {title: 'QA Complex', monthDuration: 4},
+     {title: 'FullStack', monthDuration: 7},
+     {title: 'Frontend', monthDuration: 4}
+ ];
+
+//
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу,
+// та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+
+
+
+let wrap2 = document.createElement('div');
+document.body.appendChild(wrap2);
+for (const el of coursesAndDurationArray2) {
+    let item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `
+        <h1 class = 'headling'>${el.title}</h1>
+        <p class='description'>Довжина курсу - ${el.monthDuration} мiсяцiв</p> 
+    `
+    wrap2.appendChild(item);
+}
+
+// ==========================
+//
+// - є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+//     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+//
+
+
+let simpsonsWrap = document.createElement('div');
+document.body.appendChild(simpsonsWrap);
+for (const simpson of simpsons) {
+    let member = document.createElement('div');
+    member.className ='member';
+    simpsonsWrap.appendChild(member);
+    member.innerHTML = `
+        <h2>${simpson.name} ${simpson.surname}</h2>
+        <h3> age - ${simpson.age}</h3> 
+        <p> ${simpson.info}</p> 
+        <img src="${simpson.photo}">
+    `
+    simpsonsWrap.appendChild(member);
+
+
+}
+
+
+
+
+
+// =========================
+//     Цикл в циклі
+// - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+// ------------------
+
+
+
+let coursesArrayWrap = document.createElement('div');
+coursesArrayWrap.className='coursesArrayWrap';
+document.body.appendChild(coursesArrayWrap);
+
+for (const item of coursesArray) {
+    let course = document.createElement('div');
+    course.className ='course';
+    coursesArrayWrap.appendChild(course);
+
+
+    course.innerHTML = `
+        <h2 class="course_title">${item.title}</h2>
+        <div class="course_monthDuration"> Months of Duration - ${item.monthDuration}</div> 
+        <div class="course_hourDuration"> Hours of Duration - ${item.hourDuration}</div> 
+    `
+    let modulesUl= document.createElement('ul');
+    modulesUl.className ='modulesUl';
+    course.appendChild(modulesUl);
+
+    for (const moduleElement of item.modules) {
+        let li = document.createElement('li');
+        li.innerText = moduleElement;
+        modulesUl.appendChild(li);
+
+    }
+    coursesArrayWrap.appendChild(course);
+
+
+}
